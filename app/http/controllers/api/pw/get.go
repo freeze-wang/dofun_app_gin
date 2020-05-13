893193a7a20aa2a09386363d584e1c2f3d88f9ee
freeze <wangdj@500wan.com>
2020-05-14 01:19:46 +0800

    fix

--- app/http/controllers/api/pw/get.go
+++ app/http/controllers/api/pw/get.go
@@ -10,7 +10,11 @@ import (
 func List(c *gin.Context) {
 	var data interface{}
 
-	data = dj.PwList("","","","",1,10)
+	data, err := dj.PwList(c.Param("class_id"), c.Param("attribute_id"),  c.Param("sex"),  c.Param("orderBy"), c.GetInt("page"), c.GetInt("pageSize"))
+	if err!=nil{
+		controllers.SendErrorResponse(c, err)
+		return
+	}
 	controllers.SendOKResponse(c, data)
 	return
 }
